f3c23430343e9ba5a598ab935b0f6f1e43ed9805
RTourt <revengeofnoraneko@gmail.com>
2023-11-30 15:21:51 +0100

    ✨ F01 : Login > Add title and form

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,4 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  const [inputValue, setInputValue] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Bonjour ${inputValue}`);
+    setInputValue("");
+  };
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  return (
+    <div>
+      <h1>Fuel Your Vibe</h1>
+      <h2>Connect To Tribe</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          value={inputValue}
+          onChange={handleChange}
+          type="text"
+          placeholder="Enter your name..."
+          required
+        />
+        <button>My Coffee Space</button>
+      </form>
+    </div>
+  );
 }
